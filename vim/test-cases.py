fbf8ed9d3c3315f59e9048cbfc167e53493e6b7b
Niels Okker <nielsokker@hotmai.com>
2019-10-06 17:47:08 +0200

    testcase 10 done

--- vim/test-cases.py
+++ vim/test-cases.py
@@ -165,13 +165,128 @@ def test6():
     else:
         print("Test 6 failed: result is not equal to the expected result") 
 
+def test7():
+    testFileName = 'test-7.txt'
+    tester = VimTester(r'(All|Bot|Top|\d+\%)+', r'', options, tempFolder + testFileName)
+    
+    tester.interpreter.send("O")
+    tester.getScreenContent('INSERT')
+
+    tester.interpreter.send('00000000: 4e65 7665 7220 676f 6e6e 6120 6c65 7420')
+    tester.getScreenContent( r'00000000: 4e65 7665 7220 676f 6e6e 6120 6c65 7420')
+
+    tester.interpreter.sendcontrol("c")
+    tester.getScreenContent(None)
+    
+    tester.interpreter.sendline(":%!xxd -r")
+    tester.getScreenContent('Never gonna let you down.')   
+    tester.interpreter.sendline(":w")
+    tester.interpreter.sendline(":q")
+
+    res = filecmp.cmp(tempFolder + testFileName, resultFolder + testFileName)
+    
+    if res:
+        print("Test 7 Succeeded")
+    else:
+        print("Test 7 failed: result is not equal to the expected result") 
+
+def test8():
+    testFileName = 'test-8.txt'
+    tester = VimTester(r'(All|Bot|Top|\d+\%)+', r'', options, tempFolder + testFileName)
+    
+    tester.interpreter.send("gg")
+    tester.getScreenContent("1,1")
+    tester.interpreter.send("j")
+    tester.getScreenContent("2")
+    tester.interpreter.send("gI")
+    tester.getScreenContent('INSERT')
+
+    tester.interpreter.send('Sed ut perspiciatis unde omnis')
+    tester.getScreenContent( r'Sed ut perspiciatis unde omnis')
+
+    tester.interpreter.sendcontrol("c")
+    tester.getScreenContent(None)
+
+    tester.interpreter.sendline(":w")
+    tester.interpreter.sendline(":q")
+
+    res = filecmp.cmp(tempFolder + testFileName, resultFolder + testFileName)
+    
+    if res:
+        print("Test 8 Succeeded")
+    else:
+        raise Exception("Test 8 failed: result is not equal to the expected result")
+
+def test9():
+    testFileName = 'test-9.txt'
+    tester = VimTester(r'(All|Bot|Top|\d+\%)+', r'', options, tempFolder + testFileName)
+    
+    tester.interpreter.send("gg")
+    tester.getScreenContent("1,1")
+    tester.interpreter.send("i")
+    tester.getScreenContent('INSERT')
+
+    tester.interpreter.sendcontrol("c")
+    tester.getScreenContent(None)
+
+    tester.interpreter.send("jj")
+    tester.getScreenContent("3")
+
+    tester.interpreter.send("gi")
+    tester.getScreenContent('INSERT')
+
+    tester.interpreter.send('Lorem ipsum dolor sit amet.')
+    tester.getScreenContent( r'Lorem ipsum dolor sit amet.')
+
+    tester.interpreter.sendcontrol("c")
+    tester.getScreenContent(None)
+
+    tester.interpreter.sendline(":w")
+    tester.interpreter.sendline(":q")
+
+    res = filecmp.cmp(tempFolder + testFileName, resultFolder + testFileName)
+    
+    if res:
+        print("Test 9 Succeeded")
+    else:
+        raise Exception("Test 9 failed: result is not equal to the expected result")
+
+def test10():
+    testFileName = 'test-10.txt'
+    tester = VimTester(r'(All|Bot|Top|\d+\%)+', r'', options, tempFolder + testFileName)
+    
+    tester.interpreter.send("gg")
+    tester.getScreenContent("1,1")
+    tester.interpreter.send("2s")
+    tester.getScreenContent('INSERT')
+
+    tester.interpreter.send('Hello world!')
+    tester.getScreenContent( r'Hello world!')
+
+    tester.interpreter.sendcontrol("c")
+    tester.getScreenContent(None)
+
+    tester.interpreter.sendline(":w")
+    tester.interpreter.sendline(":q")
+
+    res = filecmp.cmp(tempFolder + testFileName, resultFolder + testFileName)
+    
+    if res:
+        print("Test 10 Succeeded")
+    else:
+        raise Exception("Test 10 failed: result is not equal to the expected result")
 try:
-    #test1()
-    #test2()
-    #test3()
-    #test4()
-    #test5()
-    test6()
+    # test1()
+    # test2()
+    # test3()
+    # test4()
+    # test5()
+    # test6()
+    # test7()
+    # test8()
+    # test9()
+    test10()
+
     
 except Exception as identifier:
     print("tests failed")
