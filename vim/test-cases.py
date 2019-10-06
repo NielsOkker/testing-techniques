e52f6ca7d746f512831743be882a527101cd2515
unsigned-decimal <afakir@live.nl>
2019-10-06 14:52:02 +0200

    Test 1 done

--- vim/test-cases.py
+++ vim/test-cases.py
@@ -1,18 +1,21 @@
 from vimtester import VimTester, TIMEOUT
 import time
 import filecmp
+from distutils.dir_util import copy_tree
+
 
 options = {}
 options['geometry'] = (25, 100)
 options['timeout'] = 1
 
-inputFolder = '../startfiles/'
-resultFolder = '../resultfiles/'
-tempFolder = '../tempfiles/'
+inputFolder = './startfiles/'
+resultFolder = './resultfiles/'
+tempFolder = './tempfiles/'
+copy_tree(inputFolder, tempFolder)
 
 def test1():
     testFileName = 'test-1.txt'
-    tester = VimTester(r'(All|Bot|Top|\d+\%)+', r'', options, testFileName)
+    tester = VimTester(r'(All|Bot|Top|\d+\%)+', r'', options, tempFolder + testFileName)
     tester.interpreter.send("i")
     tester.getScreenContent('INSERT')
 
@@ -22,11 +25,17 @@ def test1():
     tester.interpreter.sendcontrol("c")
     tester.getScreenContent(None)
 
-    tester.interpreter.sendline(":wq")
+    tester.interpreter.sendline(":w")
+    tester.interpreter.sendline(":q")
 
-    filecmp.cmp(inputFolder + testFileName, resultFolder + testFileName)
-
-    print("tests succeeded")
+    res = filecmp.cmp(tempFolder + testFileName, resultFolder + testFileName)
+    
+    if res:
+        print("Test 1 Succeeded")
+    else:
+        print("Test 1 failed: result is not equal to the expected result")
+    
+    
     
 
 try:
