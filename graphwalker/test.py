cbfc6d7e56b44362edf3a9e25303aaded88d4656
Niels Okker <nielsokker@hotmai.com>
2019-11-11 12:32:41 +0100

    done

--- graphwalker/test.py
+++ graphwalker/test.py
@@ -1,33 +1,83 @@
 import json
 import subprocess
 
+from vimtester import VimTester, TIMEOUT
+import time
+import filecmp
+from distutils.dir_util import copy_tree
+import traceback
+import os
+
+options = {}
+options['geometry'] = (25, 100)
+options['timeout'] = 2
+
+tempFolder = './tempfiles/'
+
 def dejsonfy(jsonArray):
     array = []
     for i in jsonArray:
         array.append(i['currentElementName'])
     return array
 
-def handle_currentElement(ce):
+def handle_currentElement(ce, tester):
+    print("Handle", ce)
     if ce == "v_Normal":
-        print("Handle v_Normal")
+        tester.getScreenContent()
     elif ce == "v_Insert":
-        print("Handle v_Insert")
+        tester.getScreenContent('INSERT')
     elif ce == "e_Ctrl-C":
-        print("Handle e_Ctrl")
+        tester.interpreter.sendcontrol("c")
     elif ce == "e_Esc":
-        print("Handle e_Esc")
+        tester.interpreter.sendcontrol("c")
+    elif ce == "e_i":
+        tester.interpreter.send("i")
+    elif ce == "e_I":
+        tester.interpreter.send("I")
+    elif ce == "e_a":
+        tester.interpreter.send("a")
+    elif ce == "e_A":
+        tester.interpreter.send("A")
+    elif ce == "e_o":
+        tester.interpreter.send("o")
+    elif ce == "e_O":
+        tester.interpreter.send("O")
+    elif ce == "e_gI":
+        tester.interpreter.send("gI")
+    elif ce == "e_gi":
+        tester.interpreter.send("gi")
+    elif ce == "e_s":        
+        tester.interpreter.send("2s") 
+    elif ce == "e_S":        
+        tester.interpreter.send("3S") 
+    elif ce == "e_cc":
+        tester.interpreter.send("cc")
+    elif ce == "e_C":
+        tester.interpreter.send("C")
     else:
-        print("Handle Insert command")
+        print("Unknown action or state", ce)
 
 
-#Start
-var = subprocess.check_output(['java', '-jar', 'graphwalker-cli-4.0.1.jar', 'offline', '-m', \
-    'undefined.json', '"quick_random(edge_coverage(100))"'])
+def run_graphwalker_test(name_of_model, typeoftest):
+    gw_output = subprocess.check_output(['java', '-jar', 'graphwalker-cli-4.0.1.jar', 'offline', '-m', \
+    name_of_model, typeoftest])
 
-var = var.splitlines()
-stateArray = []
-for v in var:
-    stateArray.append(json.loads(v)['currentElementName'])
+    element_strings = gw_output.splitlines()
+    stateArray = []
+    for element_string in element_strings:
+        stateArray.append(json.loads(element_string)['currentElementName'])
 
-for i in stateArray:
-    handle_currentElement(i)
+    testFileName = 'test-4.txt'
+    tester = VimTester(r'(All|Bot|Top|\d+\%)+', r'', options, tempFolder + testFileName)
+
+    for i in stateArray:
+        handle_currentElement(i, tester)
+
+    handle_currentElement("e_Ctrl-C", tester)
+    handle_currentElement("v_Normal", tester)
+    tester.interpreter.sendline(":w")
+    tester.interpreter.sendline(":q")
+
+    print("Succesfuly transitioned ")
+
+run_graphwalker_test('undefined.json', '"quick_random(edge_coverage(100))"')
